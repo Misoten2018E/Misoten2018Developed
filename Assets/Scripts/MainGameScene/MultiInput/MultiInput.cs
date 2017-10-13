68d5b719aa29302d6f42a016989a3b75e986814e
maedakazuhiko <omoono666@gmail..com>
2017-10-13 09:19:45 +0900

    multiinput.cs player.cs add commit

---
+++ Assets/Scripts/MainGameScene/MultiInput/MultiInput.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiInput : MonoBehaviour {
+    public int PlayerNo;
+    private int WorkPlayerNo;
+
+    // Use this for initialization
+    void Start () {
+        if (PlayerNo < 0 || PlayerNo > 3)
+        {
+            WorkPlayerNo = 0;
+        }
+        else
+        {
+            WorkPlayerNo = PlayerNo;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //======================================================================
+    public float GetXaxis()
+    {
+        return Input.GetAxisRaw("Player"+ WorkPlayerNo + "_X");
+    }
+
+    public float GetYaxis()
+    {
+        return Input.GetAxisRaw("Player" + WorkPlayerNo + "_Y");
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetButtonCircleTrigger()
+    {
+        return Input.GetButtonDown("Player" + WorkPlayerNo + "_Circle");
+    }
+
+    public bool GetButtonCircleRelease()
+    {
+        return Input.GetButtonUp("Player" + WorkPlayerNo + "_Circle");
+    }
+
+    public bool GetButtonCirclePress()
+    {
+        return Input.GetButton("Player" + WorkPlayerNo + "_Circle");
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetButtonTriangleTrigger()
+    {
+        return Input.GetButtonDown("Player" + WorkPlayerNo + "_Triangle");
+    }
+
+    public bool GetButtonTriangleRelease()
+    {
+        return Input.GetButtonUp("Player" + WorkPlayerNo + "_Triangle");
+    }
+
+    public bool GetButtonTrianglePress()
+    {
+        return Input.GetButton("Player" + WorkPlayerNo + "_Triangle");
+    }
+    //======================================================================
+    
+    //======================================================================
+    public bool GetButtonSquareTrigger()
+    {
+        return Input.GetButtonDown("Player" + WorkPlayerNo + "_Square");
+    }
+
+    public bool GetButtonSquareRelease()
+    {
+        return Input.GetButtonUp("Player" + WorkPlayerNo + "_Square");
+    }
+
+    public bool GetButtonSquarePress()
+    {
+        return Input.GetButton("Player" + WorkPlayerNo + "_Square");
+    }
+    //======================================================================
+    
+    //======================================================================
+    public bool GetButtonCrossTrigger()
+    {
+        return Input.GetButtonDown("Player" + WorkPlayerNo + "_Cross");
+    }
+
+    public bool GetButtonCrossRelease()
+    {
+        return Input.GetButtonUp("Player" + WorkPlayerNo + "_Cross");
+    }
+
+    public bool GetButtonCrossPress()
+    {
+        return Input.GetButton("Player" + WorkPlayerNo + "_Cross");
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetAllButtonCircleTrigger()
+    {
+        for(int i = 0;i < 4 ;i++)
+        {
+            if(Input.GetButtonDown("Player" + i + "_Circle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonCircleRelease()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonUp("Player" + i + "_Circle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonCirclePress()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButton("Player" + i + "_Circle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetAllButtonTriangleTrigger()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonDown("Player" + i + "_Triangle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonTriangleRelease()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonUp("Player" + i + "_Triangle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonTrianglePress()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButton("Player" + i + "_Triangle"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetAllButtonSquareTrigger()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonDown("Player" + i + "_Square"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonSquareRelease()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonUp("Player" + i + "_Square"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonSquarePress()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButton("Player" + i + "_Square"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //======================================================================
+
+    //======================================================================
+    public bool GetAllButtonCrossTrigger()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonDown("Player" + i + "_Cross"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonCrossRelease()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButtonUp("Player" + i + "_Cross"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool GetAllButtonCrossPress()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Input.GetButton("Player" + i + "_Cross"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    //======================================================================
+}
---
+++ Assets/Scripts/MainGameScene/Player/Player.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    MultiInput input;
+    public int no;
+    public float MoveSpeed;
+    public float RotationSpeed;
+
+    CharacterController CharCon;
+    Vector3 velocity;
+
+    // Use this for initialization
+    void Start () {
+        input = this.GetComponent<MultiInput>();
+        CharCon = this.GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(input.GetXaxis() != 0)
+        {
+            print("X" + no + input.GetXaxis());
+        }
+        if(input.GetYaxis() != 0)
+        {
+            print("Y" + no + input.GetYaxis());
+        }
+
+        velocity.x = input.GetXaxis() * MoveSpeed;
+        velocity.z = input.GetYaxis() * MoveSpeed;
+
+        Vector3 direction = new Vector3(velocity.x,0,velocity.z);
+        if(direction.sqrMagnitude > 0f)
+        {
+            Vector3 forward = Vector3.Slerp(transform.forward,direction,RotationSpeed * Time.deltaTime/Vector3.Angle(transform.forward,direction));
+            transform.LookAt(transform.position + forward);
+        }
+
+        CharCon.Move(velocity * Time.deltaTime);
+
+        //if (input.GetButtonCircleTrigger())
+        //{
+        //    print("ButtonCircleTrigger" + no);
+        //}
+
+        //if (input.GetButtonCircleRelease())
+        //{
+        //    print("GetButtonCircleRelease" + no);
+        //}
+
+        //if (input.GetButtonCirclePress())
+        //{
+        //    print("GetButtonCirclePress" + no);
+        //}
+
+
+        //if (input.GetButtonSquareTrigger())
+        //{
+        //    print("GetButtonSquareTrigger" + no);
+        //}
+
+        //if (input.GetButtonSquareRelease())
+        //{
+        //    print("GetButtonSquareRelease" + no);
+        //}
+
+        //if (input.GetButtonSquarePress())
+        //{
+        //    print("GetButtonSquarePress" + no);
+        //}
+
+
+        //if (input.GetButtonTriangleTrigger())
+        //{
+        //    print("GetButtonTriangleTrigger" + no);
+        //}
+
+        //if (input.GetButtonTriangleRelease())
+        //{
+        //    print("GetButtonTriangleRelease" + no);
+        //}
+
+        //if (input.GetButtonTrianglePress())
+        //{
+        //    print("GetButtonTrianglePress" + no);
+        //}
+
+
+        //if (input.GetButtonCrossTrigger())
+        //{
+        //    print("GetButtonCrossTrigger" + no);
+        //}
+
+        //if (input.GetButtonCrossRelease())
+        //{
+        //    print("GetButtonCrossRelease" + no);
+        //}
+
+        //if (input.GetButtonCrossPress())
+        //{
+        //    print("GetButtonCrossPress" + no);
+        //}
+
+
+        //if (input.GetAllButtonCircleTrigger())
+        //{
+        //    print("GetAllButtonCircleTrigger" + no);
+        //}
+
+        //if (input.GetAllButtonCircleRelease())
+        //{
+        //    print("GetAllButtonCircleRelease" + no);
+        //}
+
+        //if (input.GetAllButtonCirclePress())
+        //{
+        //    print("GetAllButtonCirclePress" + no);
+        //}
+
+
+        //if (input.GetAllButtonSquareTrigger())
+        //{
+        //    print("GetAllButtonSquareTrigger" + no);
+        //}
+
+        //if (input.GetAllButtonSquareRelease())
+        //{
+        //    print("GetAllButtonSquareRelease" + no);
+        //}
+
+        //if (input.GetAllButtonSquarePress())
+        //{
+        //    print("GetAllButtonSquarePress" + no);
+        //}
+
+
+        //if (input.GetAllButtonTriangleTrigger())
+        //{
+        //    print("GetAllButtonTriangleTrigger" + no);
+        //}
+
+        //if (input.GetAllButtonTriangleRelease())
+        //{
+        //    print("GetAllButtonTriangleRelease" + no);
+        //}
+
+        //if (input.GetAllButtonTrianglePress())
+        //{
+        //    print("GetAllButtonTrianglePress" + no);
+        //}
+
+
+        //if (input.GetAllButtonCrossTrigger())
+        //{
+        //    print("GetAllButtonCrossTrigger" + no);
+        //}
+
+        //if (input.GetAllButtonCrossRelease())
+        //{
+        //    print("GetAllButtonCrossRelease" + no);
+        //}
+
+        //if (input.GetAllButtonCrossPress())
+        //{
+        //    print("GetAllButtonCrossPress" + no);
+        //}
+    }
+}
