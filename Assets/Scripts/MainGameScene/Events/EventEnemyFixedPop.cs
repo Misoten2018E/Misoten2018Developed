e3fc27208c940f1c18a00d45c53bdbdae67aad9f
edo kota <kota.430@gmail.com>
2017-10-15 20:42:20 +0900

    現状の作成物を一時保存(機能実装はしていない)

---
+++ Assets/Scripts/MainGameScene/Events/EventEnemyFixedPop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventEnemyFixedPop : TimelineEventStandard {
+
+	[SerializeField] private EnemyData[] Enemy;
+
+	public override void EventStart() {
+		
+
+	}
+
+	[System.Serializable]
+	public class EnemyData {
+
+		[SerializeField] int Route1;
+		[SerializeField] int Route2;
+
+	}
+}
