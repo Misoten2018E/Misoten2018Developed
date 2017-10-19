1f32bef2cdc8ae33a44043cc1bb6b73d9b5820bd
edo kota <kota.430@gmail.com>
2017-10-19 19:07:12 +0900

    注目イベント時にカメラを移動させてその位置を知らせるように仕組み作成

--- Assets/Scripts/MainGameScene/Camera/FocusCamera.cs
+++ Assets/Scripts/MainGameScene/Camera/FocusCamera.cs
@@ -10,26 +10,117 @@ public class FocusCamera : MonoBehaviour {
 	//                                    inspector
 	//========================================================================================
 
+	[SerializeField] private float OrthoMinSize = 7;
+
+	[SerializeField] private AnimationCurve MoveEvent;
+
 	[SerializeField] private List<Transform> TargetTransform;
 
-	[SerializeField] private float OrthoMinSize = 7;
+	//========================================================================================
+	//                                    public
+	//========================================================================================
+
+
+	// シングルトンインスタンス
+	static FocusCamera myInstance;
+	static public FocusCamera Instance {
+		get {
+			return myInstance;
+		}
+	}
+
+
+	/// <summary>
+	/// ターゲットの追加
+	/// </summary>
+	/// <param name="trs"></param>
+	public void AddTarget(Transform trs) {
+
+		if (trs != null) {
+			print("ターゲット追加");
+			TargetTransform.Add(trs);
+		}
+	}
+
+	/// <summary>
+	/// ターゲットの削除
+	/// </summary>
+	/// <param name="trs"></param>
+	public void DeleteTarget(Transform trs) {
+
+		if (trs != null) {
+			print("ターゲット削除");
+			TargetTransform.Remove(trs);
+		}
+	}
+
 	//========================================================================================
 	//                                    public - override
 	//========================================================================================
 
+	private void Awake() {
+
+		myInstance = this;
+	}
+
 	// Use this for initialization
 	void Start () {
 		OffsetPos = transform.position;
+
+		NormalUpdate();
+
+		DebugInit();
 	}
-	
+
+	int DebugId = -1;
+	void DebugInit() {
+		DebugId = DebugLog.ChaseLog("");
+	}
+
+	void DebugUpdate() {
+		string s = "位置" + NowFrameData.Position.ToString() + "カメラサイズ" + NowFrameData.OrthoSize.ToString();
+		DebugLog.ChaseLog(s, DebugId);
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 
-		var mostFar = MostFarTransformPosition();
+		// 現フレームのデータ計算
+		NowFrameData.square = MostFarTransformPosition();
+		NowFrameData.Position = NowFrameData.square.Center3Dxz;
+		NowFrameData.OrthoSize = CalcCameraOrthoSize(NowFrameData.square);
 
-		OrthographicSizeUpdate(mostFar);
+		DebugUpdate();
 
-		transform.position = mostFar.Center3Dxz + OffsetPos;
+		//　そもそも大きく動くイベント中なら
+		if (isEventActive) {
+
+			// 大きく動くべきターゲットと現状を比較して更に移動が必要なら
+			if (isEventStart(NowFrameData,EventTargetData)) {
+				DebugLog.log("イベント中に更に移動する");
+				CreateEventData(NowFrameData);
+			}
+			// イベントの続行
+			else {
+				EventUpdate();
+			}
+		}
+		else {
+
+			// 前フレームと比較して大きく動く必要が有るかどうかをチェック
+			if (isEventStart(NowFrameData,OldData)) {
+				DebugLog.log("イベント開始");
+				CreateEventData(NowFrameData);
+			}
+			else {
+
+				// 大きく動く必要が無いなら微更新
+				NormalUpdate();
+			}
+		}
+
+		// 過去データ保持
+		OldData = NowFrameData;
 	}
 
 
@@ -40,18 +131,102 @@ public class FocusCamera : MonoBehaviour {
 	// 初期カメラ位置の保持
 	private Vector3 OffsetPos = new Vector3();
 
+	private NecessaryBuffData OldData = new NecessaryBuffData();
+
+	private NecessaryBuffData EventTargetData = new NecessaryBuffData();
+
+	private NecessaryBuffData NowFrameData = new NecessaryBuffData();
+
+	bool isEventActive = false;
+
+	private float DiffLength = 1f;
+	/// <summary>
+	/// 特殊更新の必要が有るかどうかをチェック
+	/// </summary>
+	/// <returns></returns>
+	bool isEventStart(NecessaryBuffData nowData ,NecessaryBuffData Target) {
+
+		if (Mathf.Abs(Target.OrthoSize - nowData.OrthoSize) > DiffLength) {
+			return true;
+		}
+
+		if (Mathf.Abs((Target.Position - nowData.Position).magnitude) > DiffLength) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+	void CreateEventData(NecessaryBuffData nowData) {
+
+		EventTargetData = nowData;
+
+		// 位置補完データ作成
+		PositionComp.Initialize(transform.position - OffsetPos, nowData.Position);
+
+		// サイズ補完データ作成
+		SizeComp.Initialize(CameraComp.orthographicSize, nowData.OrthoSize);
+
+		isEventActive = true;
+		ElapsedTime = 0f;
+	}
+
+	/// <summary>
+	/// 通常の微更新
+	/// </summary>
+	void NormalUpdate() {
+
+		OrthographicSizeUpdate(NowFrameData.square);
+		transform.position = NowFrameData.square.Center3Dxz + OffsetPos;
+	}
+
+	/// <summary>
+	/// イベント時更新
+	/// </summary>
+	void EventUpdate() {
+
+		float rate = ElapsedTime / EventTime;
+		rate = MoveEvent.Evaluate(rate);
+
+		if (rate >= 1f) {
+			rate = 1f;
+			isEventActive = false;
+			DebugLog.log("イベント終了");
+		}
+		Vector3 pos = PositionComp.CalcPosition(rate);
+		float size = SizeComp.CalcFloat(rate);
+
+		SetOrthoSize(size);
+		transform.position = pos + OffsetPos;
+
+		ElapsedTime += Time.deltaTime;
+	}
+
 	/// <summary>
 	/// カメラOrthographicサイズ更新
 	/// </summary>
 	/// <param name="mostFar"></param>
 	private void OrthographicSizeUpdate(Square mostFar) {
 
-		CameraComp.orthographicSize = CalcCameraOrthoSize(mostFar);
+		float size = CalcCameraOrthoSize(mostFar);
+		SetOrthoSize(size);
+	}
 
+	/// <summary>
+	/// 平行投影サイズのセット
+	/// </summary>
+	/// <param name="size"></param>
+	private void SetOrthoSize(float size) {
+
+		CameraComp.orthographicSize = size;
 		if (CameraComp.orthographicSize < OrthoMinSize) {
 			CameraComp.orthographicSize = OrthoMinSize;
 		}
 	}
+
+	private const float EventTime = 1f;
+	private float ElapsedTime;
 
 	/// <summary>
 	/// 中心点計算
@@ -109,7 +284,10 @@ public class FocusCamera : MonoBehaviour {
 
 		return ((lenX > lenZ) ? (lenX) : (lenZ)) / 2;
 	}
+	
 
+	Vector3Complession PositionComp = new Vector3Complession();
+	floatComplession SizeComp = new floatComplession();
 	
 
 	Camera _CameraComp;
@@ -144,5 +322,10 @@ public class FocusCamera : MonoBehaviour {
 		}
 	}
 
+	public struct NecessaryBuffData {
 
+		public Vector3 Position;
+		public Square square;
+		public float OrthoSize;
+	}
 }
