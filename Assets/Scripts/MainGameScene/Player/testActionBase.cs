333eb6ac978417ce24e5321819c92d6f1f523b8d
edo kota <kota.430@gmail.com>
2017-10-21 20:31:52 +0900

    テストプレイ用のスクリプト作成

---
+++ Assets/Scripts/MainGameScene/Player/testActionBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class testActionBase : MonoBehaviour {
+
+
+	public abstract void ActionCircle(GameObject player);
+	public abstract void ActionCross(GameObject player);
+	public abstract void ActionSquare(GameObject player);
+	public abstract void ActionTriangle(GameObject player);
+
+}
---
+++ Assets/Scripts/MainGameScene/Player/testActionHero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testActionHero : testActionBase {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+
+
+	//========================================================================================
+	//                                    public override
+	//========================================================================================
+
+	public override void ActionCircle(GameObject player) {
+		
+	}
+
+	public override void ActionCross(GameObject player) {
+		
+	}
+
+	public override void ActionSquare(GameObject player) {
+		
+	}
+
+	public override void ActionTriangle(GameObject player) {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
