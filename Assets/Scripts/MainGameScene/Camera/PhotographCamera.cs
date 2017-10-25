6e61ccd63fec840fc8278ed90770d3ac58dce017
edo kota <kota.430@gmail.com>
2017-10-25 11:39:47 +0900

    撮影用カメラ作成

---
+++ Assets/Scripts/MainGameScene/Camera/PhotographCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotographCamera : MonoBehaviour {
+
+
+	// シングルトンインスタンス
+	static PhotographCamera myInstance;
+	static public PhotographCamera Instance {
+		get {
+			return myInstance;
+		}
+	}
+
+
+	Camera _MyCamera;
+	public Camera MyCamera {
+		private set { _MyCamera = value; }
+		get { return _MyCamera; }
+	}
+      
+
+	// Use this for initialization
+	void Start() {
+		myInstance = this;
+		MyCamera = GetComponent<Camera>();
+		gameObject.SetActive(false);
+	}
+	
+
+	public Texture2D Photo(int width,int height) {
+
+		return CameraCapture.Capture(MyCamera, width, height);
+
+	}
+}
