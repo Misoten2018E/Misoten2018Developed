aef3150f611936c94ff22c561cc0a31d9006609b
edo kota <kota.430@gmail.com>
2017-11-04 19:04:04 +0900

    サウンドマネージャの最適化(3D的な鳴らし方に変更) プレイヤー攻撃可能位置を判断するエリア作成

---
+++ Assets/Scripts/MainGameScene/Enemy/CheckEnemyAttackArea.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckEnemyAttackArea : MonoBehaviour,DebuggableObject {
+
+	/// <summary>
+	/// デバッグ実行
+	/// </summary>
+	/// <param name="isDebugMode"></param>
+	public void Debug(bool isDebugMode) {
+
+		var mesh = GetComponent<MeshRenderer>();
+		if (mesh != null) {
+			mesh.enabled = isDebugMode;
+		}
+	}
+
+
+	//========================================================================================
+	//                                    public
+	//========================================================================================
+
+	public void SetHitEnterCallback(System.Action<Collider> _HitCallback) {
+		HitCallBack = _HitCallback;
+	}
+
+	public void AddHitEnterCallback(System.Action<Collider> _HitCallback) {
+		HitCallBack += _HitCallback;
+	}
+
+
+	Collider _MyCollider;
+	public Collider MyCollider {
+		private set { _MyCollider = value; }
+		get { return _MyCollider; }
+	}
+      
+
+	//========================================================================================
+	//                                    private
+	//========================================================================================
+
+	private System.Action<Collider> HitCallBack;
+
+	private void OnTriggerEnter(Collider other) {
+
+		if (HitCallBack != null) {
+			HitCallBack(other);
+			HitCallBack = null;
+		}
+	}
+}
