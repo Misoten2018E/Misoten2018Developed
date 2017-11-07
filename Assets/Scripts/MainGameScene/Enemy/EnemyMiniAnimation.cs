3b28389c12e561ed4df48d478b8e1091d332561c
edo kota <kota.430@gmail.com>
2017-11-07 15:05:51 +0900

    敵のコントローラを作る前提としたスクリプトやコントローラの作成

---
+++ Assets/Scripts/MainGameScene/Enemy/EnemyMiniAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyMiniAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//========================================================================================
+	//                                    private
+	//========================================================================================
+
+	enum AnimationType {
+		Move,
+		AttackPose,
+		Attack,
+		Down,
+		CityPose,
+		RunAway
+	}
+}
