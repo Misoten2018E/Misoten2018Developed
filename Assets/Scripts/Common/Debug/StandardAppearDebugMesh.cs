f01beb94772e0481aaeb78da9b778dc8f5a08867
edo kota <kota.430@gmail.com>
2017-11-11 18:42:57 +0900

    プレイヤの移動を制限する壁の追加 デバッグ時にしか見えないような仕組み追加

---
+++ Assets/Scripts/Common/Debug/StandardAppearDebugMesh.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class StandardAppearDebugMesh : MonoBehaviour ,DebuggableObject{
+
+	public void Debug(bool isDebugMode) {
+		var m = GetComponent<MeshRenderer>();
+	}
+}
