73585b64d0fc58c7e3103ebff77296db07eff359
edo kota <kota.430@gmail.com>
2017-11-11 18:41:02 +0900

    何かしらを知らせるためのテロップ追加

---
+++ Assets/Scripts/Common/Utility/AnimatorControllStandard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorControllStandard : MonoBehaviour {
+
+
+	//========================================================================================
+	//                                    override
+	//========================================================================================
+
+	// Use this for initialization
+	void Start () {
+		var a = Anim;
+	}
+
+
+	//========================================================================================
+	//                                    public
+	//========================================================================================
+
+	public void AnimationStart() {
+
+		gameObject.SetActive(true);
+		AnimatorStateInfo stateInfo = Anim.GetCurrentAnimatorStateInfo(0);
+		Anim.Play(stateInfo.fullPathHash, 0, 0.0f);  //初期位置に戻す
+	}
+
+	Animator _Anim;
+	public Animator Anim {
+		get {
+			if (_Anim == null) {
+				_Anim = GetComponent<Animator>();
+			}
+			return _Anim;
+		}
+	}
+}
