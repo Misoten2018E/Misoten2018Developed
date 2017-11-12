8aec881bb21a15d74d1ba731a2c92227863e31e5
edo kota <kota.430@gmail.com>
2017-11-12 18:13:26 +0900

    一部アイテムのプレハブ化 ゲームループを行えるようシーンの修正

--- Assets/Scripts/Common/Utility/AnimatorControllStandard.cs
+++ Assets/Scripts/Common/Utility/AnimatorControllStandard.cs
@@ -20,12 +20,35 @@ public class AnimatorControllStandard : MonoBehaviour {
 	//                                    public
 	//========================================================================================
 
+	/// <summary>
+	/// アニメーション開始
+	/// </summary>
 	public void AnimationStart() {
 
 		gameObject.SetActive(true);
 		AnimatorStateInfo stateInfo = Anim.GetCurrentAnimatorStateInfo(0);
 		Anim.Play(stateInfo.fullPathHash, 0, 0.0f);  //初期位置に戻す
 	}
+
+	/// <summary>
+	/// アニメーション終了時に呼ばれる
+	/// </summary>
+	public void AnimationEnd() {
+
+		if (AnimationEndCallback != null) {
+			AnimationEndCallback();
+			AnimationEndCallback = null;
+		}
+	}
+
+
+	System.Action _AnimationEndCallback;
+	public System.Action AnimationEndCallback {
+		set { _AnimationEndCallback = value; }
+		get { return _AnimationEndCallback; }
+	}
+      
+
 
 	Animator _Anim;
 	public Animator Anim {
