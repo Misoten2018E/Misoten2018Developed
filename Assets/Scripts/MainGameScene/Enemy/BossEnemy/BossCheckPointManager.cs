618d3b4a4e3ae952cc5ba09b6bcca4d915a2ac88
edo kota <kota.430@gmail.com>
2017-11-17 00:12:58 +0900

    ボス管理のための色々追加

---
+++ Assets/Scripts/MainGameScene/Enemy/BossEnemy/BossCheckPointManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCheckPointManager : MonoBehaviour {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	[SerializeField] private RelayPoint StartTransform;
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+	/// <summary>
+	/// 次のリレーポイントを返す
+	/// </summary>
+	/// <param name="point"></param>
+	/// <param name="isClockwise"></param>
+	/// <returns></returns>
+	public RelayPoint GetNextRelayPoint(RelayPoint point,bool isClockwise = false) {
+
+		// もしnullなら最初のエリアを渡す
+		if (point == null) {
+			return StartTransform;
+		}
+
+		int max = TransList.Count;
+		for (int i = 0; i < max; i++) {
+
+			if (point == TransList[i]) {
+
+				int next = isClockwise ? i + 1 : i - 1;
+				if (next < 0) {
+					next = max - 1;
+				}else if(next >= max) {
+					next = 0;
+				}
+				return TransList[next];
+			}
+		}
+
+		print("同一のものが見つからない異常値");
+		return null;
+	}
+
+	/// <summary>
+	/// 一周していたらtrue
+	/// </summary>
+	/// <param name="nextArea"></param>
+	/// <returns></returns>
+	public bool IsEndCheck(RelayPoint nextArea) {
+
+		return (nextArea == StartTransform);
+	}
+
+	//========================================================================================
+	//                                 public - override
+	//========================================================================================
+
+	void Start() {
+
+		var c = GetComponentsInChildren<RelayPoint>();
+		TransList.AddRange(c);
+	}
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	List<RelayPoint> TransList = new List<RelayPoint>();
+
+}
--- Assets/Scripts/MainGameScene/Enemy/BossEnemy/BossEnemy.cs
+++ Assets/Scripts/MainGameScene/Enemy/BossEnemy/BossEnemy.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossEnemy : MonoBehaviour {
+public class BossEnemy : MoveTargetEnemy {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	[SerializeField] private float SpownTime = 60;
+
+	
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+	public void InitBossEnemy(BossCheckPointManager mng) {
+
+	}
+
+	//========================================================================================
+	//                                 public - override
+	//========================================================================================
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+
+
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+
 	}
+
+	public override void InitEnemy(UsedInitData InitData) {
+		throw new System.NotImplementedException();
+	}
+
+	public override bool IsDeath {
+		get { return false; }
+		protected set { throw new System.NotImplementedException(); }
+	}
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+
 }
