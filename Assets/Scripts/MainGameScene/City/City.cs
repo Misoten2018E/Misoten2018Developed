866fc07274ee14ada77f1aae06c069d268c1c8ed
maedakazuhiko <omoono666@gmail.com>
2017-11-26 23:59:00 +0900

    街発展システム作成。イベントシステムにバグあり。

--- Assets/Scripts/MainGameScene/City/City.cs
+++ Assets/Scripts/MainGameScene/City/City.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class City : MonoBehaviour {
+public class City : SceneStartEvent{
 
 	// シングルトンインスタンス
 	static City myInstance;
@@ -10,7 +10,14 @@ public class City : MonoBehaviour {
 		get {
 			return myInstance;
 		}
-	}
+	}    
+
+    public List<int> ScoreList;
+    public int CityModelCnt;
+
+    Animator[] AnimatorArray;
+    int[] CityStaArray;
+    int score;
 
 
 	private void Awake() {
@@ -26,6 +33,45 @@ public class City : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
+        if (!isInitialized) return;
 
-	}
+        score += 1;
+        print("スコア" + score);
+
+        for (int i = 0;i < ScoreList.Count ;i++)
+        {
+            if (ScoreList[i] <= score)
+            {
+                CityStaArray[i] = 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        for (int i = 0;i < CityModelCnt;i++)
+        {
+            AnimatorArray[i].SetInteger("CitySta", CityStaArray[i]);
+        }
+    }
+
+    public override void SceneMyInit()
+    {
+        AnimatorArray = new Animator[CityModelCnt];
+        CityStaArray = new int[CityModelCnt];
+
+        AnimatorArray[0] = transform.Find("city1").GetComponent<Animator>();
+        AnimatorArray[1] = transform.Find("city2").GetComponent<Animator>();
+        AnimatorArray[2] = transform.Find("city3").GetComponent<Animator>();
+        AnimatorArray[3] = transform.Find("city4").GetComponent<Animator>();
+
+        for (int i = 0;i <  CityModelCnt;i++)
+        {
+            CityStaArray[i] = 0;
+        }
+        score = 0;
+
+        isInitialized = true;
+    }
 }
