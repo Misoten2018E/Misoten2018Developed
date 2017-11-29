8b94b9d831de4edb655f206a7f12507255e45303
edo kota <kota.430@gmail.com>
2017-11-29 10:19:29 +0900

    中型敵管理系のスクリプト作成

---
+++ Assets/Scripts/MainGameScene/Enemy/MinEnemy/EnemyMediumAnimation.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyMediumAnimation : ControlAnimatorEnemy {
+
+
+	//========================================================================================
+	//                                    public - override
+	//========================================================================================
+
+	// Use this for initialization
+	void Start () {
+
+		NowType = AnimationType.Move;
+	}
+
+
+	//========================================================================================
+	//                                    public
+	//========================================================================================
+
+	
+	public void Animation(AnimationType type) {
+
+		transform.localRotation = Quaternion.identity;
+		transform.localPosition = Vector3.zero;
+
+		switch (type) {
+			case AnimationType.Move:
+				SetState((int)AnimationType.Move);
+				AnimEnd = null;
+				break;
+
+			case AnimationType.AttackPose:
+				AttackPose();
+				break;
+			case AnimationType.Attack1:
+				SetState((int)AnimationType.Attack1);
+				Attack();
+				break;
+			case AnimationType.Attack2:
+				SetState((int)AnimationType.Attack2);
+				Attack();
+				break;
+			case AnimationType.Attack3:
+				SetState((int)AnimationType.Attack3);
+				Attack();
+				break;
+			case AnimationType.RunAway:
+				SetState((int)AnimationType.RunAway);
+				AnimEnd = null;
+				break;
+			case AnimationType.Damage:
+				AnimEnd = AnimationDamageEnd;
+				break;
+			
+			default:
+				break;
+		}
+
+		NowType = type;
+	}
+
+	private void AnimationDamageEnd() {
+
+		SetState((int)AnimationType.Move);
+		base.DamageEnd();
+	}
+
+	/// <summary>
+	/// アニメーション終了時コールバック
+	/// </summary>
+	/// <param name="callback"></param>
+	public void SetAnimationEndCallback(System.Action callback) {
+
+		AnimEnd = callback;
+	}
+
+	//========================================================================================
+	//                                    private
+	//========================================================================================
+
+	public enum AnimationType {
+		Move,
+		AttackPose,
+		Attack1,
+		Attack2,
+		Attack3,
+		RunAway,
+		Damage,
+	}
+
+	AnimationType _nowType;
+	public AnimationType NowType {
+		private set { _nowType = value; }
+		get { return _nowType; }
+	}
+
+	//System.Action AnimEndCallback;
+
+	/// <summary>
+	/// 攻撃開始モーション終了
+	/// </summary>
+	//void AttackAnimationPoseEnd() {
+
+	//	if (AnimEndCallback != null) {
+	//		AnimEndCallback();
+	//		AnimEndCallback = null;
+	//	}
+	//}
+
+	///// <summary>
+	///// 攻撃モーション終了
+	///// </summary>
+	//void AttackAnimationEnd() {
+	//	if (AnimEndCallback != null) {
+	//		AnimEndCallback();
+	//		AnimEndCallback = null;
+	//	}
+	//}
+
+	///// <summary>
+	///// ダメージモーション終了
+	///// </summary>
+	//void DamageAnimationEnd() {
+
+	//	if (AnimEndCallback != null) {
+	//		AnimEndCallback();
+	//		AnimEndCallback = null;
+	//	}
+	//}
+}
---
+++ Assets/Scripts/MainGameScene/Enemy/MinEnemy/MediumEnemy.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GroupLeaderEnemyCommand))]
+public class MediumEnemy : AimingPlayerEnemy {
+
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	[SerializeField] private float GroupCheckTime = 0.5f;
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+	//========================================================================================
+	//									   override
+	//========================================================================================
+
+	protected override void Start() {
+		base.Start();
+	}
+
+	public override void InitEnemy(UsedInitData InitData) {
+
+		transform.position = InitData.BasePosition.position;
+		MyType = EnemyType.Middle;
+		EnemyManager.Instance.SetEnemy(this);
+	}
+
+	/// <summary>
+	/// 攻撃ポーズ
+	/// </summary>
+	protected override void AnimationAttackPose() {
+		ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.AttackPose);
+	}
+
+	/// <summary>
+	/// 攻撃アニメーション
+	/// </summary>
+	protected override void AnimationAttack() {
+
+		switch (AttackId) {
+
+			case 0:
+				ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.Attack1);
+				myTrail.StartTrail(TrailSupport.BodyType.RightArm);
+				break;
+
+			case 1:
+				ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.Attack2);
+				myTrail.StartTrail(TrailSupport.BodyType.LeftArm);
+				break;
+
+			case 2:
+				ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.Attack3);
+				myTrail.StartTrail(TrailSupport.BodyType.RightLeg);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	/// <summary>
+	/// 移動アニメーション
+	/// </summary>
+	protected override void AnimationMove() {
+		ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.Move);
+		myTrail.EndTrail();
+	}
+
+	/// <summary>
+	/// ダメージアニメーション
+	/// </summary>
+	protected override void AnimationDamaged() {
+		ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.Damage);
+		myTrail.EndTrail();
+	}
+
+	/// <summary>
+	/// 避難アニメーション
+	/// </summary>
+	protected override void AnimationRunAway() {
+		ChildModelMedium.Animation(EnemyMediumAnimation.AnimationType.RunAway);
+		myTrail.EndTrail();
+	}
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	/// <summary>
+	/// グループを組むかどうか
+	/// </summary>
+	/// <returns></returns>
+	private bool CheckGroupFormation() {
+
+		int rand = Random.Range(0, 100);
+		return (rand <= GroupFormRate);
+	}
+
+	/// <summary>
+	/// 攻撃体勢に入る
+	/// </summary>
+	private void AttackPoseMedium() {
+
+		SelectAttackId();
+
+		var prefab = GetResource();
+		MyAttackObj = Instantiate(prefab);
+		MyAttackObj.Initialize(this.gameObject);
+
+		IsAttacking = true;
+		AnimationAttackPose();
+
+		if (ieAttackMode != null) {
+			StopCoroutine(ieAttackMode);
+		}
+		ieAttackMode = AttackStart(0.5f + attackIntervalTime);
+		StartCoroutine(ieAttackMode);
+
+		StopMove(2f + NextAttackInterval);
+	}
+
+	/// <summary>
+	/// 攻撃当たり判定プレハブゲット
+	/// </summary>
+	/// <returns></returns>
+	private HitSeriesofAction GetResource() {
+
+		HitSeriesofAction prefab = null;
+
+		switch (AttackId) {
+
+			case 0:
+				prefab = ResourceManager.Instance.Get<HitSeriesofAction>(ConstDirectry.DirPrefabsHitPlayer, ConstActionHitData.ActionHeroWeak1);
+				break;
+
+			case 1:
+				prefab = ResourceManager.Instance.Get<HitSeriesofAction>(ConstDirectry.DirPrefabsHitPlayer, ConstActionHitData.ActionHeroWeak2);
+				break;
+
+			case 2:
+				prefab = ResourceManager.Instance.Get<HitSeriesofAction>(ConstDirectry.DirPrefabsHitPlayer, ConstActionHitData.ActionHeroWeak3);
+				break;
+
+			default:
+				break;
+		}
+		return prefab;
+	}
+
+	/// <summary>
+	/// 攻撃タイプの選択
+	/// </summary>
+	private void SelectAttackId() {
+		AttackId = Random.Range(0, 2);
+	}
+
+	int AttackId;
+
+	int _GroupFormRate;
+	public int GroupFormRate {
+		set {
+			_GroupFormRate = value;
+			_GroupFormRate = Mathf.Clamp(_GroupFormRate, 0, 100);
+		}
+		get { return _GroupFormRate; }
+	}
+      
+
+	EnemyMediumAnimation _ChildModelMedium;
+	/// <summary>
+	/// 中型用のキャッシュ
+	/// </summary>
+	public EnemyMediumAnimation ChildModelMedium {
+		private set { _ChildModelMedium = value; }
+		get { return _ChildModelMedium; }
+	}
+}
