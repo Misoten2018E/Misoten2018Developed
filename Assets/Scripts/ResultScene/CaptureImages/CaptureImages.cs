a743b884c09941161a89ab173ce8a81edad1cfd3
edo kota <kota.430@gmail.com>
2017-12-01 21:20:18 +0900

    リザルトでキャプチャ画像を表示できるように仕組み作成

---
+++ Assets/Scripts/ResultScene/CaptureImages/CaptureImages.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptureImages : MonoBehaviour {
+
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+
+	//========================================================================================
+	//                                    override
+	//========================================================================================
+
+	// Use this for initialization
+	void Start() {
+
+		var r = GetComponentsInChildren<RawImage>();
+		imageList.AddRange(r);
+
+		CaptureImageSet();
+	}
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	private void CaptureImageSet() {
+
+		var inst = CheckProducePhotoCamera.Instance;
+
+#if UNITY_DEBUG
+		if (inst == null) {
+			return;
+		}
+#endif
+
+		var caplist = inst.PhotoList;
+
+		for (int i = 0; i < caplist.Count; i++) {
+			imageList[i].texture = caplist[i];
+		}
+	}
+
+
+
+
+
+
+	List<RawImage> imageList = new List<RawImage>();
+}
