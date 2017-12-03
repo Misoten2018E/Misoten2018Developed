1458956ca9491532ddc8df0d4fa51f12744a02bf
edo kota <kota.430@gmail.com>
2017-12-03 22:21:43 +0900

    撮影時にデバッグで画像が表示されるように

---
+++ Assets/Scripts/Common/Debug/DebugCaptureImages.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCaptureImages : MonoBehaviour, DebuggableObject {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	[SerializeField] RawImage image;
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+	public static void SetImages(Texture2D tex) {
+#if UNITY_DEBUG
+		myInstance.image.texture = tex;
+#endif
+	}
+
+
+	// シングルトンインスタンス
+	static DebugCaptureImages myInstance;
+	static public DebugCaptureImages Instance {
+		get {
+			return myInstance;
+		}
+	}
+
+
+	//========================================================================================
+	//                                    override
+	//========================================================================================
+
+	public void Debug(bool isDebugMode) {
+		gameObject.SetActive(isDebugMode);
+	}
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	private void Start() {
+		myInstance = this;
+	}
+
+	
+}
