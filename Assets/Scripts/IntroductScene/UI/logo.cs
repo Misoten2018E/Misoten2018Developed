8251e9771bf5a53c85f175f23066663251f175e0
maedakazuhiko <omoono666@gmail.com>
2017-12-09 02:27:44 +0900

    Characterの動きのAnimation作成。UIタイトルロゴとカメラも。

---
+++ Assets/Scripts/IntroductScene/UI/logo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logo : MonoBehaviour {
+
+    Animator ani;
+    MultiInput Minput;
+
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+        Minput = GetComponent<MultiInput>();
+
+        ani.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Minput.GetAllButtonCircleTrigger())
+        {
+            ani.enabled = true;
+        }
+	}
+}
