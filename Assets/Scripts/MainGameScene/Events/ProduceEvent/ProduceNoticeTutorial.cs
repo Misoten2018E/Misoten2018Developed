f0d9c323d8963a275b13a3f58616e9ba86022988
edo kota <kota.430@gmail.com>
2017-12-09 22:20:38 +0900

    チュートリアルのテキスト管理作成

---
+++ Assets/Scripts/MainGameScene/Events/ProduceEvent/ProduceNoticeTutorial.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceNoticeTutorial : ProduceEventBase {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+	[Tooltip("使用する演出オブジェクトをセット")]
+	[SerializeField] TutorialWindowController NoticeWindow;
+
+	[SerializeField] private TutorialWindowController.AnimType StartAnimation = TutorialWindowController.AnimType.Appear;
+	[SerializeField] private TutorialWindowController.AnimType EndAnimation = TutorialWindowController.AnimType.Disappear;
+
+	//[TextArea(1,3)]
+	//[SerializeField] private string Texts;
+
+	//[SerializeField] private Sprite SpriteTexture;
+
+	//[Tooltip("どちらかが0なら初期設定を利用する")]
+	//[SerializeField] private Vector2 SpriteSize = new Vector2(0, 0);
+
+	[Tooltip("表示時間")]
+	[SerializeField] private float DisplayTime = 2f;
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+	//========================================================================================
+	//                                 public - override
+	//========================================================================================
+
+	public override void ProduceStart() {
+
+		NoticeWindow.gameObject.SetActive(true);
+		NoticeWindow.Animation(StartAnimation);
+
+		//NoticeWindow.TextArea = Texts;
+		//NoticeWindow.ImageArea = SpriteTexture;
+
+		//if (SpriteSize.x != 0f && SpriteSize.y != 0f) {
+		//	NoticeWindow.ImageSize = SpriteSize;
+		//}
+
+		StartCoroutine(GameObjectExtensions.DelayMethod(DisplayTime, EndProduce));
+	}
+
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	private void EndProduce() {
+
+		NoticeWindow.Animation(EndAnimation);
+	}
+
+}
