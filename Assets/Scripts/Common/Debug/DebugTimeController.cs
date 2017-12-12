7435f2dd8938986430050c5e7bf44246fd486f59
edo kota <kota.430@gmail.com>
2017-12-12 22:59:11 +0900

    ゲーム起動時にも適応されるよう変更

--- Assets/Scripts/Common/Debug/DebugTimeController.cs
+++ Assets/Scripts/Common/Debug/DebugTimeController.cs
@@ -6,4 +6,14 @@ public class DebugTimeController : MonoBehaviour {
 
 	[Range(0f, 10f)][Tooltip("時間を倍速させる(1なら通常)")]
 	[SerializeField] public float TimeSpeed = 1f;
+
+#if UNITY_DEBUG
+
+	private void Awake() {
+
+		Time.timeScale = TimeSpeed;
+
+	}
+
+#endif
 }
