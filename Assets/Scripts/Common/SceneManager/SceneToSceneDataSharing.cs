7a88335eaec3678a674d2ba71d32ee5b06741d6e
edo kota <kota.430@gmail.com>
2017-12-13 18:23:02 +0900

    データ引き継ぎ用オブジェクトの追加

---
+++ Assets/Scripts/Common/SceneManager/SceneToSceneDataSharing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneToSceneDataSharing : MonoBehaviour {
+
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+
+	// シングルトンインスタンス
+	static SceneToSceneDataSharing myInstance;
+	static public SceneToSceneDataSharing Instance {
+		get {
+			return myInstance;
+		}
+	}
+
+
+	MainToResult _mainToResultData = new MainToResult();
+	/// <summary>
+	/// メインからリザルトへ渡すデータ群
+	/// </summary>
+	public MainToResult mainToResultData {
+		private set { _mainToResultData = value; }
+		get { return _mainToResultData; }
+	}
+      
+
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+
+	public struct MainToResult {
+		// プレイヤー最終ステータス群
+		public int Player1_LastSta;
+		public int Player2_LastSta;
+		public int Player3_LastSta;
+		public int Player4_LastSta;
+
+		// 街レベル
+		public int CityLevel;
+
+		public List<Texture2D> textureLists;
+	}
+}
