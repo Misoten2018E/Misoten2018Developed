904459782aef9c4c683b941fde116587093079e9
edo kota <kota.430@gmail.com>
2017-12-19 00:33:17 +0900

    雑魚出現位置調整のプレハブ作成

---
+++ Assets/Scripts/MainGameScene/Enemy/BossPopPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPopPosition : MonoBehaviour {
+
+	[SerializeField] List<Transform> Positions;
+
+
+	List<Transform> _PositionList;
+	public List<Transform> PositionList {
+		private set { _PositionList = value; }
+		get { return _PositionList; }
+	}
+      
+}
---
+++ Assets/Scripts/MainGameScene/Enemy/BossPopPositionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPopPositionManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		var poses = GetComponentsInChildren<BossPopPosition>();
+		PositionList.AddRange(poses);
+		AccessNum = 0;
+	}
+
+	/// <summary>
+	/// 一度アクセスされる度に次のものを返す
+	/// </summary>
+	/// <returns></returns>
+	public BossPopPosition BossPopEnemyPosition() {
+
+		if (AccessNum >= PositionList.Count) {
+			print("設定異常");
+			DebugLog.log("設定異常");
+			return PositionList[0];
+		}
+
+		AccessNum++;
+		return PositionList[AccessNum - 1];
+
+	}
+
+	int AccessNum = 0;
+
+	List<BossPopPosition> PositionList = new List<BossPopPosition>();
+}
