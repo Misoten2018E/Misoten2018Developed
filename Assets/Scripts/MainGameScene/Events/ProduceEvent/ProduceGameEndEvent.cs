8b35587ac80c2dac10a08c45ad5a81fa58341859
edo kota <kota.430@gmail.com>
2018-01-06 00:24:31 +0900

    ボス退治イベントなどの微調整とプレイヤーがエラーはくのを回避

---
+++ Assets/Scripts/MainGameScene/Events/ProduceEvent/ProduceGameEndEvent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceGameEndEvent : MonoBehaviour,IFGameEndEvent,IFGameEndProduceCheck {
+
+
+	//========================================================================================
+	//                                    inspector
+	//========================================================================================
+
+
+	//========================================================================================
+	//                                     public
+	//========================================================================================
+
+
+	//========================================================================================
+	//                                    override
+	//========================================================================================
+
+	// Use this for initialization
+	void Start() {
+
+		EventManager<IFGameEndEvent>.Instance.SetObject(this);
+		EventManager<IFGameEndProduceCheck>.Instance.SetObject(this);
+		isEndCheck = false;
+	}
+
+	public void GameEnd() {
+
+		var boss = FindObjectOfType<BossEnemy>();
+
+		CreateDestroyParticle(boss.transform.position);
+
+		CameraManager.Instance.EventCamera.EventCameraStartWithRot(ProduceEventCamera.CameraEvent.BossDestroy, boss.transform, new Vector3(0, 10f, 0), new Vector3(0f, 180f, 0f));
+
+		StartCoroutine(GameObjectExtensions.DelayMethod(1.5f, EndEvent));
+	}
+
+	public bool IsEndProduce() {
+		return isEndCheck;
+	}
+	//========================================================================================
+	//                                     private
+	//========================================================================================
+
+	bool isEndCheck;
+
+	private void EndEvent() {
+
+		isEndCheck = true;
+
+	}
+
+	private void CreateDestroyParticle(Vector3 pos) {
+
+		var despar = ResourceManager.Instance.Get<ParticleSupport>(ConstDirectry.DirParticleEdo, ConstEffects.DestroyBoss);
+		var p = Instantiate(despar);
+		p.transform.position = pos;
+	}
+}
