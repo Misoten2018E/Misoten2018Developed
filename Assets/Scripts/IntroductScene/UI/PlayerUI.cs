687fc2c14f37d83f3c51b8c1184f2c4eb31cfeb6
edo kota <kota.430@gmail.com>
2018-01-06 11:42:56 +0900

    タイトルの調整

--- Assets/Scripts/IntroductScene/UI/PlayerUI.cs
+++ Assets/Scripts/IntroductScene/UI/PlayerUI.cs
@@ -1,31 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour {
-    public Texture2D tex;
 
-    MultiInput Minput;
-    UnityEngine.UI.Image image;
+	MultiInput Minput;
+
+	[SerializeField] Image deleteImage;
 
     // Use this for initialization
     void Start () {
         Minput = GetComponent<MultiInput>();
-        image = GetComponent<UnityEngine.UI.Image>();
-        image.enabled = false;
+		MyAnim.gameObject.SetActive(false);
+		NowState = -1;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Minput.GetAllButtonCircleTrigger())
-        {
-            image.enabled = true;
-        }
 
-        if (Minput.GetButtonCircleTrigger())
+        if (Minput.GetAllButtonCircleTrigger() && NowState == -1)
         {
-            image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
-            SoundManager.Instance.PlaySE(SoundManager.SEType.TitlePlayerOK,new Vector3(0,0,0));
-        }
+			MyAnim.gameObject.SetActive(true);
+			SetAnimation(AnimType.Appear);
+			NowState = 1;
+		}
+
+        if (Minput.GetButtonCircleTrigger() && NowState == 1)
+        {
+			SetAnimation(AnimType.Change);
+			SoundManager.Instance.PlaySE(SoundManager.SEType.TitlePlayerOK,new Vector3(0,0,0));
+			NowState = 2;
+
+		//	StartCoroutine(GameObjectExtensions.DelayMethod(0.3f, () => { deleteImage.gameObject.SetActive(false); }));
+		}
+
+		//if (NowState == 0) {
+		//	NowState = 1;
+		//}
 	}
+
+
+	void SetAnimation(AnimType type) {
+
+		MyAnim.SetInteger(StateStr, (int)type);
+		MyAnim.SetTrigger(ActiveStr);
+	}
+
+	public enum AnimType {
+		Appear,
+		Change,
+	}
+
+	int NowState = 0;
+
+	const string StateStr = "State";
+	const string ActiveStr = "isActivate";
+
+	Animator _MyAnim;
+	public Animator MyAnim {
+		get {
+			if (_MyAnim == null) {
+				_MyAnim = GetComponentInChildren<Animator>();
+			}
+			return _MyAnim;
+		}
+	} 
 }
