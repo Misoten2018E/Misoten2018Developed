b062cf41f20fa04b4b7f0e0b058250650dec91de
maedakazuhiko <omoono666@gmail.com>
2018-01-21 01:36:42 +0900

    Result写真順番入れ替え。枠追加。

--- Assets/Scripts/ResultScene/CaptureImages/CaptureImages.cs
+++ Assets/Scripts/ResultScene/CaptureImages/CaptureImages.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class CaptureImages : MonoBehaviour {
 
     Animator ani;
-
+    
 	//========================================================================================
 	//                                    inspector
 	//========================================================================================
@@ -28,8 +28,28 @@ public class CaptureImages : MonoBehaviour {
 
     private void Awake()
     {
-        var r = GetComponentsInChildren<RawImage>();
-        imageList.AddRange(r);
+        //var r = GetComponentsInChildren<RawImage>();
+        //imageList.AddRange(r);
+        //backList.AddRange(r);
+        imageList = new RawImage[8];
+        backList = new RawImage[8];
+        imageList[0] = transform.Find("Image1").GetComponent<RawImage>();
+        imageList[1] = transform.Find("Image2").GetComponent<RawImage>();
+        imageList[2] = transform.Find("Image3").GetComponent<RawImage>();
+        imageList[3] = transform.Find("Image4").GetComponent<RawImage>();
+        imageList[4] = transform.Find("Image5").GetComponent<RawImage>();
+        imageList[5] = transform.Find("Image6").GetComponent<RawImage>();
+        imageList[6] = transform.Find("Image7").GetComponent<RawImage>();
+        imageList[7] = transform.Find("Image8").GetComponent<RawImage>();
+
+        backList[0] = transform.Find("back1").GetComponent<RawImage>();
+        backList[1] = transform.Find("back2").GetComponent<RawImage>();
+        backList[2] = transform.Find("back3").GetComponent<RawImage>();
+        backList[3] = transform.Find("back4").GetComponent<RawImage>();
+        backList[4] = transform.Find("back5").GetComponent<RawImage>();
+        backList[5] = transform.Find("back6").GetComponent<RawImage>();
+        backList[6] = transform.Find("back7").GetComponent<RawImage>();
+        backList[7] = transform.Find("back8").GetComponent<RawImage>();
 
         CaptureImageSet();
 
@@ -56,7 +76,7 @@ public class CaptureImages : MonoBehaviour {
 
         if (caplist == null)
         {
-            for (int i = 0; i < imageList.Count; i++)
+            for (int i = 0; i < 8; i++)
             {
                 imageList[i].enabled = false;
             }
@@ -64,10 +84,11 @@ public class CaptureImages : MonoBehaviour {
             return;
         }
 
-		for (int i = 0; i < imageList.Count; i++) {
+		for (int i = 0; i < 8; i++) {
             if (i >= caplist.Count)
             {
                 imageList[i].enabled = false;
+                backList[i].enabled = false;
 
                 continue;
             }
@@ -79,19 +100,27 @@ public class CaptureImages : MonoBehaviour {
             
 
             imageList[i].enabled = false;
+            backList[i].enabled = false;
         }
 	}
 
     public void CaptureON()
     {
-        for (int i = 0; i < imageList.Count; i++)
+        for (int i = 0; i < 8; i++)
         {
          
             if (imageList[i].texture != null)
             {
                 imageList[i].enabled = true;
+                backList[i].enabled = true;
             }
 
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            print("name"+ imageList[i].name + imageList[i].enabled);
+            print("name" + backList[i].name + backList[i].enabled);
         }
 
         ani.enabled = true;
@@ -100,5 +129,8 @@ public class CaptureImages : MonoBehaviour {
 
 
 
-    List<RawImage> imageList = new List<RawImage>();
+    //List<RawImage> imageList = new List<RawImage>();
+    //List<RawImage> backList = new List<RawImage>();
+    RawImage[] imageList;
+    RawImage[] backList;
 }
